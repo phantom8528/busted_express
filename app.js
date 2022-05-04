2fbcbafe3459b00198fcda89451e6953fb47418c
Gabriel Jefferson <phantom8528@gmail.com>
2022-05-04 10:49:12 -0400

    Fix 5-6 in app.js: replaced comma with semicolon, made port and hostname lowercase

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const http = require('http'); //<-- Fix 2: Declared http
 const hostname = '127.0.0.1';
 const port = 3333;
 
-const express = require('express'),
-const app = express() //<-- Fix 3: Assigned all express functionality to app variable
+const express = require('express'); //<-- Fix 5: Replaced comma with semicolon
+const app = express(); //<-- Fix 3: Assigned all express functionality to app variable
 
 const es6Renderer = require('express-es6-template-engine'); //<-- Fix 4: Applied "const" to the beginning of the variable
 
-const app = express();
 
 app.engine('html', es6Renderer);
 app.set('views', './views');
@@ -16,7 +15,7 @@ app.set('view engine', 'html');
 
 const server = http.createServer(app);
 
-server.listen(PORT, HOSTNAME, () => {
+server.listen(port, hostname, () => { // <-- Fix 6: made port and hostname lowercase
     console.log(`Server running at http://${hostname}:${port}`)
 });
 
