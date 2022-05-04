80daf817760172e68e4ad2dca1184d120367d35e
Gabriel Jefferson <phantom8528@gmail.com>
2022-05-04 10:43:31 -0400

    Fix 2-4 in app.js: Declared http, created instance of express, applied const to es6Render variable

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
-
+const http = require('http'); //<-- Fix 2: Declared http 
 const hostname = '127.0.0.1';
 const port = 3333;
 
 const express = require('express'),
-    es6Renderer = require('express-es6-template-engine');
+const app = express() //<-- Fix 3: Assigned all express functionality to app variable
+
+const es6Renderer = require('express-es6-template-engine'); //<-- Fix 4: Applied "const" to the beginning of the variable
 
 const app = express();
 
